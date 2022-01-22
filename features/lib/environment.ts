faac787defc09b967be098c287fe2941b60c178f
Pedro Piñera <pedro@ppinera.es>
2022-01-22 13:50:18 +0100

    Add a utility for running processes from acceptance tests

--- features/lib/environment.ts
+++ features/lib/environment.ts
@@ -1,3 +1,8 @@
+/**
+ * Given an environment variable, it returns true if its value represents a truthy value.
+ * @param value The environment variable value or undefined if it's not defined.
+ * @returns True if the variable represents a truthy value.
+ */
 function isTruthy(value: string | undefined): boolean {
   if (!value) {
     return false;
@@ -5,4 +10,5 @@ function isTruthy(value: string | undefined): boolean {
   return ['TRUE', 'true', 'YES', 'yes', '1'].includes(value);
 }
 
+/** Returns true if acceptance tests are run with DEBUG=1 */
 export const isDebug = isTruthy(process.env.DEBUG);
