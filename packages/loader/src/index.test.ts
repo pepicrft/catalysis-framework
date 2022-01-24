cd9acbe291ca77dc9720bd53a611b03283439a0f
Pedro Piñera <pedro@craftweg.com>
2022-01-24 16:57:35 +0100

    Add loader package

---
+++ packages/loader/src/index.test.ts
@@ -0,0 +1,9 @@
+import {assert, expect, test} from 'vitest';
+
+// Edit an assertion and save to see HMR in action
+
+test('Math.sqrt()', () => {
+  expect(Math.sqrt(4)).toBe(2);
+  expect(Math.sqrt(144)).toBe(12);
+  expect(Math.sqrt(2)).toBe(Math.SQRT2);
+});
