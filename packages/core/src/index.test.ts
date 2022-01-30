3dddc6d63fc5a69dbef8ca204d11c062dfd6d361
Pedro Piñera <pedro@ppinera.es>
2022-01-30 19:06:33 +0100

    I removed Typescript project references because tools like Vitest fail to resolve them. Moreover, VSCode doesn't rely on a doing a first compilation in a clean environment for auto-complete and suggestions to work

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -1,8 +1,10 @@
 import {assert, expect, test} from 'vitest';
+import {something as anotherThing} from '@gestaltjs/support';
 
 // Edit an assertion and save to see HMR in action
 
 test('Math.sqrt()', () => {
+  anotherThing();
   expect(Math.sqrt(4)).toBe(2);
   expect(Math.sqrt(144)).toBe(12);
   expect(Math.sqrt(2)).toBe(Math.SQRT2);
