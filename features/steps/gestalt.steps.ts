a19d332ca94892c232e43b1ab97b4ceb0a4e5d4c
Ascen Salmerón <ascensalmanez@gmail.com>
2022-02-01 08:58:58 +0100

    Fix on gestalt.steps file

--- features/steps/gestalt.steps.ts
+++ features/steps/gestalt.steps.ts
@@ -2,13 +2,19 @@ import {exec} from 'child_process';
 
 import path from 'pathe';
 import {Then} from 'cucumber';
+import { gestaltDevPath } from '../lib/constants';
+
+Then('I should be able to see the Gestalt help menu', function() {
+  const command = `${gestaltDevPath} --help`;
+  exec(command);
+});
 
 Then(
   /I should be able to run (.+) successfully in the working directory/,
   function (command) {
     const gestaltExecutable = path.join(
       __dirname,
-      '../../packages/gestalt/bin/gestalt-dev',
+      gestaltDevPath,
     );
     exec(`${gestaltExecutable} ${command} ${this.temporaryDirectory}`);
   },
