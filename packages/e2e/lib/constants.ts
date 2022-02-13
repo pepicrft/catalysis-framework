4788ba67ec623f341ad2949d11c587535f9632e3
Raquel Barbadillo <mrbarbadillo@gmail.com>
2022-02-13 10:52:01 +0100

    Add create.feature to e2e tests

--- packages/e2e/lib/constants.ts
+++ packages/e2e/lib/constants.ts
@@ -1,1 +1,6 @@
 import path from 'pathe'
+
+export const createAppDevPath = path.join(
+  __dirname,
+  '../../packages/create-app/bin/dev'
+)
--- packages/e2e/steps/gestalt.steps.ts
+++ packages/e2e/steps/gestalt.steps.ts
@@ -1,6 +1,16 @@
-import { Then } from 'cucumber'
+import { Then, When } from 'cucumber'
+import { exec } from '../lib/system'
+import { createAppDevPath } from '../lib/constants'
 
 Then('I should be able to see the Gestalt help menu', function () {
-    // const command = `${gestaltDevPath} --help`;
-    // exec(command);
+  // const command = `${gestaltDevPath} --help`;
+  // exec(command);
+})
+
+When(/I create an app named (.+) in the working directory/, function (name) {
+  exec(`${createAppDevPath} --name=${name} --path=${this.temporaryDirectory}`)
+})
+
+Then(/I should be able to (.+) the (.+) app/, function (command, name) {
+  exec(`cd ${this.temporaryDirectory}/${name} && pnpm ${command} `)
 })
