e8edd3a50f31a2422a5271bafadaa11b5714e415
Pedro <pedro@craftweg.com>
2022-02-20 20:02:21 +0100

    Add Pino dependencies and make them external

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,7 +1,16 @@
-import path from 'pathe';
-import dts from "rollup-plugin-dts";
+import path from 'pathe'
+import dts from 'rollup-plugin-dts'
 
-import {external, plugins, distDir} from '../../configurations/rollup.config';
+import { external, plugins, distDir } from '../../configurations/rollup.config'
+
+/**
+ * "piano" and "piano-pretty" use NodeJS thread workers would require custom
+ * bundling logic to respect some of their modules' structure and instruct
+ * the runtime on where the bundled modules live.
+ * To keep things simple, we are treating those as external dependencies that
+ * are installed as transitive dependency of @gestaltjs/core
+ */
+const coreExternal = [...external, 'pino', 'pino-pretty']
 
 const configuration = () => [
   {
@@ -14,7 +23,22 @@ const configuration = () => [
       },
     ],
     plugins: plugins(__dirname),
-    external: [...external],
+    external: coreExternal,
+  },
+  {
+    input: path.join(__dirname, 'src/cli/pino/development-transport.ts'),
+    output: [
+      {
+        file: path.join(
+          distDir(__dirname),
+          'cli/pino/development-transport.mjs'
+        ),
+        format: 'esm',
+        exports: 'auto',
+      },
+    ],
+    plugins: plugins(__dirname),
+    external: coreExternal,
   },
   {
     input: path.join(__dirname, 'src/framework/index.ts'),
@@ -26,8 +50,8 @@ const configuration = () => [
       },
     ],
     plugins: plugins(__dirname),
-    external: [...external],
-  }
-];
+    external: coreExternal,
+  },
+]
 
-export default configuration;
+export default configuration
