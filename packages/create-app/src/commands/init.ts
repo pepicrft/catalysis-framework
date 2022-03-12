d73dbdc644830d91ad0778ced38c1e73ebf17dd2
Raquel Barbadillo <mrbarbadillo@gmail.com>
2022-03-12 19:09:21 +0100

    Fix import gestalt => gestaltjs

--- packages/create-app/src/commands/init.ts
+++ packages/create-app/src/commands/init.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process'
-import gestaltPackage from '../../../gestalt/package.json'
+import gestaltPackage from '../../../gestaltjs/package.json'
 
 import { Command, Flags } from '@oclif/core'
 import { path } from '@gestaltjs/core/cli'
@@ -21,7 +21,7 @@ export default class Init extends Command {
   async run(): Promise<void> {
     const { args, flags } = await this.parse(Init)
     const projectPath = flags.path ? path.resolve(flags.path) : process.cwd()
-    const plopfilePath = path.resolve('') // absolute path to dist?
+    const plopfilePath = path.resolve('dist') // absolute path to dist?
     this.log(`Creating ${args.name} app at path ${projectPath}`)
     execSync(
       `cd ${plopfilePath} && plop -- --name ${args.name} --path ${projectPath} --version ${gestaltPackage.version}`
