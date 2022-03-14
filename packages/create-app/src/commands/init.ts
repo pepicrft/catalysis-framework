4eadbf6c474e8132810c753e7af8bd2ab32dec6f
Raquel Barbadillo <mrbarbadillo@gmail.com>
2022-03-14 22:34:41 +0100

    Fix plopfile path

--- packages/create-app/src/commands/init.ts
+++ packages/create-app/src/commands/init.ts
@@ -1,8 +1,12 @@
 import { execSync } from 'child_process'
 import gestaltPackage from '../../../gestaltjs/package.json'
-
 import { Command, Flags } from '@oclif/core'
 import { path } from '@gestaltjs/core/cli'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default class Init extends Command {
   static description = 'Create a Gestalt project'
   static flags = {
@@ -21,7 +25,7 @@ export default class Init extends Command {
   async run(): Promise<void> {
     const { args, flags } = await this.parse(Init)
     const projectPath = flags.path ? path.resolve(flags.path) : process.cwd()
-    const plopfilePath = path.resolve(path.dirname(''), 'dist/template')
+    const plopfilePath = path.resolve(__dirname)
     this.log(`Creating ${args.name} app at path ${projectPath}`)
     execSync(
       `cd ${plopfilePath} && plop -- --name ${args.name} --path ${projectPath} --version ${gestaltPackage.version}`
