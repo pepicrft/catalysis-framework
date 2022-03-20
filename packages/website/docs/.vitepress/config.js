be3ac3e7687befab8a94d6ce707837fd5c1499c7
Pedro <pedro@craftweg.com>
2022-03-20 06:51:06 +0100

    Add description

---
+++ packages/website/docs/.vitepress/config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'GestaltJS',
+  description:
+    'An opinionted and batteriees-included Javascript framework for building web and desktop apps.',
+})
