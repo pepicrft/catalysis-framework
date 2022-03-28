fd1b76072bc617cc8709ae5c253f7c87a4e2f4aa
Pedro Piñera Buendía <pedro@craftweg.com>
2022-03-28 17:53:04 +0200

    Fix Algolia data

--- packages/website/docs/.vitepress/config.js
+++ packages/website/docs/.vitepress/config.js
@@ -32,7 +32,7 @@ export default defineConfig({
     editLinkText: 'Edit this page on GitHub',
     lastUpdated: 'Last Updated',
     algolia: {
-      apiKey: 'd1be651e2aa2a456406755d4983dad22',
+      apiKey: '77c0d7b65508f8626260896fcb575ed5',
       indexName: 'gestaltjs',
     },
     nav: [
