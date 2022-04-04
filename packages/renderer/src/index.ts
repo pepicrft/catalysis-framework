fd74eb2e4420940f1f3bb2d98e46663a4db6ba8c
Pedro Piñera Buendía <pedro@craftweg.com>
2022-04-04 17:22:30 +0200

    Add defineRenderer function

---
+++ packages/renderer/src/index.ts
@@ -0,0 +1,26 @@
+import type { Renderer } from './renderer'
+
+export { Renderer }
+
+type RendererArg = Renderer | (() => Promise<Renderer>) | (() => Renderer)
+
+/**
+ * A utility function to define new renderers. Since the function has its argument and
+ * return value typed, when using it from the renderer default module, editors will
+ * offer a better editing experience with syntax highlighting, validation, documentation,
+ * and auto-completion.
+ *
+ * Alternatively, renderers can use the '@type' annotation:
+ *   @type {import('@gestaltjs/renderer').Renderer}
+ *   const config = {...}
+ *
+ * @param renderer {Renderer | () => Promise<Renderer> | () => Renderer} Define a new renderer.
+ * @returns A promise that resolves with the renderer.
+ */
+export async function defineRenderer(renderer: RendererArg): Promise<Renderer> {
+  if (typeof renderer === 'function') {
+    return await renderer
+  } else {
+    return renderer
+  }
+}
