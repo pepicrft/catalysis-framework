606b54eb176de3206904d6a081150178ccbbf17a
Pedro Piñera Buendía <pedro@craftweg.com>
2022-04-24 17:55:49 +0200

    Test plugin-vue

--- packages/plugin-vue/src/renderer/client.test.ts
+++ packages/plugin-vue/src/renderer/client.test.ts
@@ -1,5 +1,24 @@
-import { test, expect } from 'vitest'
+import { describe, test, expect, vi } from 'vitest'
+import { createSSRApp } from 'vue'
+import vueRenderer from './client'
+import { ClientRenderer } from '@gestaltjs/plugins'
 
-test('it works', () => {
-  expect(true).toEqual(true)
+vi.mock('vue')
+
+describe('hydrate', () => {
+  test('delegates the creation and mounting to Vue', async () => {
+    // Given
+    const renderer = vueRenderer as ClientRenderer
+    const component: any = vi.fn()
+    const domElement: any = vi.fn()
+    const app: any = { mount: vi.fn() }
+    vi.mocked(createSSRApp).mockReturnValue(app)
+
+    // When
+    await renderer.hydrate(component, domElement)
+
+    // Then
+    expect(createSSRApp).toHaveBeenCalledWith(component)
+    expect(app.mount).toHaveBeenCalledWith(domElement)
+  })
 })
---
+++ packages/plugin-vue/src/renderer/server.test.ts
@@ -0,0 +1,28 @@
+import { describe, test, expect, vi } from 'vitest'
+import { createSSRApp } from 'vue'
+import vueRenderer from './server'
+import { renderToString } from 'vue/server-renderer'
+import { ServerRenderer } from '@gestaltjs/plugins'
+
+vi.mock('vue')
+vi.mock('vue/server-renderer')
+
+describe('hydrate', () => {
+  test('delegates the creation and mounting to Vue', async () => {
+    // Given
+    const renderer = vueRenderer as ServerRenderer
+    const component: any = vi.fn()
+    const app: any = { mount: vi.fn() }
+    const html = '<div>Test</div>'
+    vi.mocked(createSSRApp).mockReturnValue(app)
+    vi.mocked(renderToString).mockResolvedValue(html)
+
+    // When
+    const got = await renderer.render(component)
+
+    // Then
+    expect(got.html).toEqual(html)
+    expect(createSSRApp).toHaveBeenCalledWith(component)
+    expect(renderToString).toHaveBeenCalledWith(app, {})
+  })
+})
