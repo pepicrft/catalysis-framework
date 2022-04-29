1b8834a00c479ce60bb623bbff76044af4f60ab3
Pedro Piñera Buendía <pedro@craftweg.com>
2022-04-29 17:27:43 +0200

    Add catalysis

--- fixtures/react-project/gestalt.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfiguration } from 'gestaltjs/configuration'
-
-// eslint-disable-next-line import/no-default-export
-export default defineConfiguration({
-  name: 'React Project',
-})
--- fixtures/react-project/targets/main/gestalt.org/gestalt.target.ts
+++
@@ -1,6 +0,0 @@
-import { defineUserMainTarget } from 'gestaltjs/target'
-
-// eslint-disable-next-line import/no-default-export
-export default defineUserMainTarget({
-  platforms: ['web'],
-})
