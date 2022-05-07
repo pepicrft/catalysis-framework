3f029d67cef91d5e7df0d2d4b5221a80400b133b
Pedro Piñera Buendía <pedro@craftweg.com>
2022-05-07 16:55:05 +0200

    Test context

---
+++ packages/core/src/shared/context.test.ts
@@ -0,0 +1,43 @@
+import { setContext, _useContext } from './context'
+import { describe, test, expect } from 'vitest'
+
+describe('setContext', () => {
+  test('serializes the context as a JSON and sets it to the env', () => {
+    // Given
+    const env: { GESTALT_INTERNAL_CONTEXT?: string } = {}
+
+    // When
+    setContext({ environment: 'production' }, env)
+
+    // Then
+    expect(
+      JSON.parse(env.GESTALT_INTERNAL_CONTEXT as string).environment
+    ).toEqual('production')
+  })
+})
+
+describe('useContext', () => {
+  test('returns the context if it exists', () => {
+    // Given
+    const env: { GESTALT_INTERNAL_CONTEXT?: string } = {
+      GESTALT_INTERNAL_CONTEXT: JSON.stringify({ environment: 'production' }),
+    }
+
+    // When
+    const got = _useContext(env)
+
+    // Then
+    expect(got.environment).toEqual('production')
+  })
+
+  test('returns a default context if it has not been set', () => {
+    // Given
+    const env = {}
+
+    // When
+    const got = _useContext(env)
+
+    // Then
+    expect(got.environment).toEqual('development')
+  })
+})
--- packages/core/src/shared/context.ts
+++ packages/core/src/shared/context.ts
@@ -2,6 +2,10 @@
  * The context is used to expose information to manifest files at loading time.
  * For example, plugins can change their setting depending on whether the project
  * is running in development or production.
+ *
+ * Because manifest files are loaded through Vite's virtual module graph, they don't
+ * have access to Gestalt's module graph, and therefore we can't share the context via
+ * an in-memory variable. To workaround that, we use the process.env global variable.
  */
 export type Context = {
   /** The environment used for running the project */
@@ -10,10 +14,24 @@ export type Context = {
 
 /**
  * Sets the context. This method should be called before loading the project.
- * @param context {Context} Sets a context before loading the project to be able to expose the conte
+ * @param context {Context} Sets a context before loading the project to be able to expose the context.
+ * @param env {NodeJS.ProcessEnv} The Node environment to set the context.
  */
-export function setContext(context: Context) {
-  process.env.GESTALT_CONTEXT = JSON.stringify(context)
+export function setContext(context: Context, env = process.env) {
+  env.GESTALT_INTERNAL_CONTEXT = JSON.stringify(context)
+}
+
+/**
+ * An internal version of useContext for testing purposes.
+ * This removes the need for mocking the global process variable.
+ */
+export function _useContext(env = process.env): Context {
+  const envVariable = env.GESTALT_INTERNAL_CONTEXT
+  if (envVariable) {
+    return JSON.parse(envVariable)
+  } else {
+    return { environment: 'development' }
+  }
 }
 
 /**
@@ -21,10 +39,5 @@ export function setContext(context: Context) {
  * @returns {Context} The shared context.
  */
 export function useContext(): Context {
-  const envVariable = process.env.GESTALT_CONTEXT
-  if (envVariable) {
-    return JSON.parse(envVariable)
-  } else {
-    return { environment: 'development' }
-  }
+  return _useContext(process.env)
 }
