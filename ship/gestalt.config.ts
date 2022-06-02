3a73bfc44bf432992dcad24da110fee2cd9fa690
Pedro Piñera Buendía <pedro@craftweg.com>
2022-06-02 17:24:45 +0200

    Fix some orphan references

--- ship/gestalt.config.ts
+++ ship/gestalt.config.ts
@@ -1,8 +1,7 @@
 import { defineConfiguration } from 'gestaltjs/configuration'
-import reactPlugin from '@gestaltjs/gestalt-plugin-react'
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfiguration({
   name: 'Ship',
-  plugins: [reactPlugin()],
+  plugins: [],
 })
