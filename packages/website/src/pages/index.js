ef21ca7cd1cbc9158fc7a2c1b2553183c00cee1b
Pedro Piñera Buendía <pedro@craftweg.com>
2022-06-20 19:34:32 +0200

    Update the title

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -34,8 +34,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`A framework for crafters`}
+      description="Build digital experiences with web technologies"
     >
       <HomepageHeader />
       <main>
