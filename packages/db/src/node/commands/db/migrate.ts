a8771e0607c478acb331e0f8ce15cda3728698f1
Pedro Piñera <pedro@ppinera.es>
2022-07-03 12:55:27 +0200

    Change @gestaltjs/db to use tsc

--- packages/db/src/node/commands/db/migrate.ts
+++ packages/db/src/node/commands/db/migrate.ts
@@ -1,5 +1,6 @@
-import { dbLogger } from '../../logger'
+import { dbLogger } from '../../logger.js'
 import { GestaltCommand } from '@gestaltjs/core/node/command'
+import { Flags } from '@oclif/core'
 
 // eslint-disable-next-line import/no-default-export
 export default class Migrate extends GestaltCommand {
