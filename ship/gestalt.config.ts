9bfc7e90e0f0d8bd094d8be0f3959644c98066d8
Pedro Piñera Buendía <pedro@craftweg.com>
2022-08-30 19:12:24 +0200

    Remove ship

--- ship/gestalt.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfiguration } from 'gestaltjs/configuration'
-
-// eslint-disable-next-line import/no-default-export
-export default defineConfiguration({
-  name: 'Ship',
-  plugins: [],
-})
