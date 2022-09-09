3f2b8b4ca970eb10df1f2984d9f9f06ed4dfafb9
Pedro Piñera Buendía <pedro@craftweg.com>
2022-09-08 17:32:57 -0700

    Fix nx setup

--- e2e/lib/random.ts
+++ e2e/lib/random.ts
@@ -4,5 +4,13 @@
  * @returns The random string.
  */
 export function randomString(length = 36): string {
-  return Math.random().toString(length).slice(2, 7)
+  let s = ''
+  const randomchar = function () {
+    const n = Math.floor(Math.random() * 62)
+    if (n < 10) return n //1-10
+    if (n < 36) return String.fromCharCode(n + 55) //A-Z
+    return String.fromCharCode(n + 61) //a-z
+  }
+  while (s.length < length) s += randomchar()
+  return s
 }
