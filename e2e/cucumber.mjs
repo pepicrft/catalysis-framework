78474b6ed4771bb71a8c5bef842b91c837ca8909
Pedro Piñera Buendía <pedro@craftweg.com>
2022-09-08 17:18:21 -0700

    Fix cucumber configuration

--- e2e/cucumber.mjs
+++ e2e/cucumber.mjs
@@ -2,25 +2,12 @@ const featureToRun = process.env.FEATURE
 
 const common = {
   import: ['world/**/*.ts', 'steps/**/*.ts', 'lib/**/*.ts'],
+  formatOptions: { colorsEnabled: true },
 }
 
 // eslint-disable-next-line import/no-default-export
 export default {
   ...common,
   format: ['progress', '@cucumber/pretty-formatter'],
+  paths: featureToRun ? [featureToRun] : ['features/**/*.feature'],
 }
-
-// const common = [
-//   '--require world/**/*.ts',
-//   '--require steps/**/*.ts',
-//   '--require lib/**/*.ts',
-//   '--format-options \'{"colorsEnabled": true}\'',
-// ]
-// if (featureToRun) {
-//   common.push(featureToRun)
-// } else {
-//   common.push('features/**/*.feature')
-// }
-
-// // eslint-disable-next-line import/no-default-export
-// export default common.join('')
