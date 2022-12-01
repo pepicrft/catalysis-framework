a40b2c9c63674c7907d4c2574acdd4c34277c2f1
Shipit <shipit@shopify.com>
2022-12-01 16:13:31 +0000

    Add esbuild's integration test

--- packages/core/src/private/node/build-system/compiler/esbuild.integration.test.ts
+++ packages/core/src/private/node/build-system/compiler/esbuild.integration.test.ts
@@ -2,46 +2,39 @@ import { describe, beforeEach, test, expect } from 'vitest'
 import { ESBuildBaseCompiler } from './esbuild.js'
 import { inTemporarydirectory } from '../../../../internal/node/testing/temporary'
 import { writeFile } from '../../../../public/node/fs.js'
-import { ModuleCompilationError } from '../compiler.js'
 
-// let subject: ESBuildBaseCompiler
+let subject: ESBuildBaseCompiler
 
-// beforeEach(() => {
-//   subject = new ESBuildBaseCompiler()
-// })
+beforeEach(() => {
+  subject = new ESBuildBaseCompiler()
+})
 
-describe('buildAndLoadModule', () => {
-  test('it returns a ModuleCompilationError error if the build fails', async () => {
-    // await inTemporarydirectory(async (tmpDir) => {
-    //   // Given
-    //   const modulePath = tmpDir.pathAppendingComponent('module.ts')
-    //   await writeFile(
-    //     modulePath,
-    //     `export function foo( invalid { return "bar" }`
-    //   )
-    //   // When
-    //   const got = (await subject.buildAndLoadModule(modulePath)).error
-    //   // Then
-    //   expect(got).toBeInstanceOf(ModuleCompilationError)
-    // })
+describe('compile', () => {
+  test('it compiles when the input module is valid', async () => {
+    await inTemporarydirectory(async (tmpDir) => {
+      // Given
+      const inputPath = tmpDir.pathAppendingComponent('input.ts')
+      const outputPath = tmpDir.pathAppendingComponent('output.ts')
+
+      await writeFile(inputPath, `export function foo(): string {}`)
+
+      // When/Then
+      await subject.compile(inputPath, outputPath)
+    })
   })
 
-  test("it loads the module when it's valid", async () => {
-    // await inTemporarydirectory(async (tmpDir) => {
-    //   // Given
-    //   const modulePath = tmpDir.pathAppendingComponent('module.ts')
-    //   await writeFile(
-    //     modulePath,
-    //     `
-    // export function foo(): string {
-    //     return "bar"
-    // }
-    // `
-    //   )
-    //   // When
-    //   const got = (await subject.buildAndLoadModule(modulePath)).value
-    //   // Then
-    //   expect(got.foo()).toEqual('bar')
-    // })
+  test('it throws when the input module is invalid', async () => {
+    await inTemporarydirectory(async (tmpDir) => {
+      // Given
+      const inputPath = tmpDir.pathAppendingComponent('input.ts')
+      const outputPath = tmpDir.pathAppendingComponent('output.ts')
+
+      await writeFile(inputPath, `export function foo( invalid {`)
+
+      // When/Then
+      await expect(async () => {
+        await subject.compile(inputPath, outputPath)
+      }).rejects.toThrowError()
+    })
   })
 })
