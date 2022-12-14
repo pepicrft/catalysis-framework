204e5187c3fa9cffb2343d4be1bf056a78d1af96
Pedro <pedro@craftweg.com>
2022-12-14 20:39:39 +0100

    Remove the ESLint plugin

--- packages/eslint-plugin/src/index.ts
+++
@@ -1,9 +0,0 @@
-import recommendedConfig from './configs/recommended'
-
-// eslint-disable-next-line import/no-default-export
-export default {
-  configs: {
-    /** Recommended configuration for Gestalt projects */
-    recommended: recommendedConfig,
-  },
-}
