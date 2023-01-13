68d252b67aca00da172e4925ef5db9fd8389b747
Pedro <pedro@craftweg.com>
2023-01-13 18:59:29 +0100

    Add @catalysisdev/common/codable module

---
+++ .changeset/wicked-drinks-retire.md
@@ -0,0 +1,5 @@
+---
+'@catalysisdev/core': minor
+---
+
+Add '@catalysisdev/core/common/codable' module to decode objects from external representations
---
+++ packages/core/src/public/common/codable.ts
@@ -0,0 +1,4 @@
+import { z } from 'zod'
+
+/** A function to define an schema to decode and validate an object from an external representation. */
+export const defineSchema = z
